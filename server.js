f9e21bad46ea252dbb0a76d6308808545e56f44a
mwierda <wierda.mckenzie@gmail.com>
2021-07-17 17:47:55 -0500

    mac/1 Add node_modules to gitignore and use .Router in mock server

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const mocks = require( 'ronin-mocks' )
 
 const server = ronin.server()
 
-server.use( '/', mocks.server( server.router(), false, true ) )
+server.use( '/', mocks.server( server.Router(), false, true ) )
 server.start()
